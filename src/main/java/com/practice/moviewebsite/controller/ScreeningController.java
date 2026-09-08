68c6539355dbef70da2e1b5a116e0be47664797e
Balazs <bercesbalu@gmail.com>
2026-09-08 20:27:01 +0200

    ScreeningController

---
+++ src/main/java/com/practice/moviewebsite/controller/ScreeningController.java
@@ -0,0 +1,36 @@
+package com.practice.moviewebsite.controller;
+
+import com.practice.moviewebsite.dto.ScreeningRequest;
+import com.practice.moviewebsite.model.Screening;
+import com.practice.moviewebsite.service.ScreeningService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/screenings")
+public class ScreeningController {
+
+    private final ScreeningService screeningService;
+
+    public ScreeningController(ScreeningService screeningService) {
+        this.screeningService = screeningService;
+    }
+
+    @GetMapping
+    public List<Screening> getAllScreenings() {
+        return screeningService.getAllScreenings();
+    }
+
+    @GetMapping("/{id}")
+    public Screening getScreeningById(@PathVariable Long id) {
+        return screeningService.getScreeningById(id);
+    }
+
+    @PostMapping
+    public Screening addScreening(
+            @RequestBody ScreeningRequest request
+    ) {
+        return screeningService.addScreening(request);
+    }
+}
