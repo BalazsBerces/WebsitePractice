b80e242a3aa76bacd6c5655b3bb6ebed7317b351
Balazs <bercesbalu@gmail.com>
2026-09-08 23:18:26 +0200

    exceptions2

--- src/main/java/com/practice/moviewebsite/exception/NotEnoughSeatsException.java
+++ src/main/java/com/practice/moviewebsite/exception/NotEnoughSeatsException.java
@@ -3,11 +3,7 @@ package com.practice.moviewebsite.exception;
 public class NotEnoughSeatsException extends RuntimeException {
 
     public NotEnoughSeatsException(int requested, int available) {
-        super(
-                "Not enough available seats. Requested: "
-                        + requested
-                        + ", available: "
-                        + available
+        super("Not enough available seats. Requested: " + requested + ", available: " + available
         );
     }
 }
