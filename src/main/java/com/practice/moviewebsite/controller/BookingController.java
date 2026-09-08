84b13cd2a381b6a87fc239accacc32c263f3bed6
Balazs <bercesbalu@gmail.com>
2026-09-08 20:34:16 +0200

    BookingController

---
+++ src/main/java/com/practice/moviewebsite/controller/BookingController.java
@@ -0,0 +1,31 @@
+package com.practice.moviewebsite.controller;
+
+import com.practice.moviewebsite.dto.BookingRequest;
+import com.practice.moviewebsite.model.Booking;
+import com.practice.moviewebsite.service.BookingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @PostMapping
+    public Booking addBooking(
+            @RequestBody BookingRequest request
+    ) {
+        return bookingService.addBooking(request);
+    }
+}
