c6678a722d02c6270b5f38ff68eecd781542533a
Balazs <bercesbalu@gmail.com>
2026-09-08 20:13:00 +0200

    bookingRequest

---
+++ src/main/java/com/practice/moviewebsite/dto/BookingRequest.java
@@ -0,0 +1,35 @@
+package com.practice.moviewebsite.dto;
+
+public class BookingRequest {
+
+    private Long screeningId;
+    private String customerName;
+    private int ticketCount;
+
+    public BookingRequest() {
+    }
+
+    public Long getScreeningId() {
+        return screeningId;
+    }
+
+    public void setScreeningId(Long screeningId) {
+        this.screeningId = screeningId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+
+    public void setTicketCount(int ticketCount) {
+        this.ticketCount = ticketCount;
+    }
+}
