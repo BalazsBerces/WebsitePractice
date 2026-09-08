5dea74fa0ff08d845eb468a8f7114cf60c5c6380
Balazs <bercesbalu@gmail.com>
2026-09-08 13:08:21 +0200

    teszt3

--- src/main/java/com/practice/moviewebsite/service/MovieService.java
+++ src/main/java/com/practice/moviewebsite/service/MovieService.java
@@ -14,7 +14,7 @@ public class MovieService {
     public MovieService(MovieRepository movieRepository){
         this.movieRepository = movieRepository;
         }
-    //asdasdasdasd
+    //asdasdasdasdasd
 
     public List<Movie> getMovies(){
         return movieRepository.findAll();
