58e967e4a60d34a870d0cd414185f8345653ca11
Balazs <bercesbalu@gmail.com>
2026-09-08 19:50:16 +0200

    dto

---
+++ src/main/java/com/practice/moviewebsite/dto/ScreeningRequest.java
@@ -0,0 +1,37 @@
+package com.practice.moviewebsite.dto;
+
+import java.time.LocalDateTime;
+
+public class ScreeningRequest {
+
+    private LocalDateTime startTime;
+    private Long movieId;
+    private Long roomId;
+
+    public ScreeningRequest() {
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+}
